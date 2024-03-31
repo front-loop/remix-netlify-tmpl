52c84302a5c9eeb47d1c4127d449db0c0a99b0fa
mancuoj <mancuoj@gmail.com>
2024-03-31 18:05:22 +0800

    feat: more deps

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,25 @@
-import { MetaFunction } from '@remix-run/node'
+import { Link } from '@nextui-org/react'
+import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
+  return [{ title: 'Remix 💚 Netlify' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
 
 export default function Index() {
   return (
     <div className="flex h-dvh items-center justify-center">
-      <h1 className="font-serif text-3xl text-slate-700">
+      <h1 className="font-serif text-3xl">
         Welcome to{' '}
-        <a target="_blank" href="https://remix.run/docs" rel="noreferrer" className="hover:underline">
-          Remix@Netlify
-        </a>
+        <Link
+          className="text-3xl"
+          href="https://remix.run/docs"
+          color="foreground"
+          underline="hover"
+          isExternal
+          showAnchorIcon
+        >
+          Remix
+        </Link>
       </h1>
     </div>
   )
