54fdaa6d60a6b9a71e3ddc38c83a4594a2079f53
mancuoj <mancuoj@gmail.com>
2024-04-01 22:38:17 +0800

    fix: singleton prisma

--- app/lib/prisma.server.ts
+++ app/lib/prisma.server.ts
@@ -1,19 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-
 declare global {
-  var prisma: PrismaClient
+  var __prisma: PrismaClient
 }
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-  prisma.$connect()
+if (!global.__prisma) {
+  global.__prisma = new PrismaClient()
 }
 
-export { prisma }
+global.__prisma.$connect()
+
+export const prisma = global.__prisma
