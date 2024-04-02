e85b3fd46ccf7c64072413ec90216e160d74b610
mancuoj <mancuoj@gmail.com>
2024-04-03 01:50:53 +0800

    feat: remix theme toggle

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,41 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-import { LinksFunction, MetaFunction } from '@remix-run/node'
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react'
+import { LinksFunction, LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from 'remix-themes'
+import { themeSessionResolver } from '~/sessions.server'
 import { cn } from '~/lib/utils'
 import stylesheet from '~/styles/globals.css?url'
 
-export const meta: MetaFunction = () => {
-  return [{ title: 'Remix @ Netlify' }, { name: 'description', content: 'Welcome to Remix!' }]
-}
+export const meta: MetaFunction = () => [{ title: 'Remix @ Netlify' }]
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }]
 
-export default function App() {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request)
+  return {
+    theme: getTheme(),
+  }
+}
+
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>()
   return (
-    <html lang="en">
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  )
+}
+
+export function App() {
+  const data = useLoaderData<typeof loader>()
+  const [theme] = useTheme()
+
+  return (
+    <html lang="en" className={cn(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body className={cn('min-h-dvh font-sans antialiased')}>
---
+++ app/sessions.server.tsx
@@ -0,0 +1,18 @@
+import { createCookieSessionStorage } from '@remix-run/node'
+import { createThemeSessionResolver } from 'remix-themes'
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: 'theme',
+    path: '/',
+    httpOnly: true,
+    sameSite: 'lax',
+    secrets: ['s3cr3t'],
+    // Set domain and secure only if in production
+    ...(isProduction ? { domain: 'remix-tmpl.netlify.app', secure: true } : {}),
+  },
+})
+
+export const themeSessionResolver = createThemeSessionResolver(sessionStorage)
